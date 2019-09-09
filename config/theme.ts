8503ad4229793ccb0a67e9eec7c6874339d39062
PKAQ <pkaq@msn.com>
2019-09-09 17:39:09 +0800

    侧边栏样式修改

--- config/theme.ts
+++ config/theme.ts
@@ -1,5 +1,6 @@
 export default {
   '@primary-color': '#1890FF',
-  '@layout-body-background': '#171717',
+  '@layout-body-background': '#252526',
   '@background-color-base': '#262626',
+  '@layout-header-backgroun': '#252526',
 };
