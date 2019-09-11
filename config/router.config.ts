2d488864f5d19f31658791b18fbf3d100014a35f
PKAQ <pkaq@msn.com>
2019-09-11 21:37:33 +0800

    fix https://github.com/ant-design/ant-design-pro/issues/5248

--- config/router.config.ts
+++ config/router.config.ts
@@ -2,7 +2,7 @@ export default [
   // app
   {
     path: '/',
-    component: '../layouts/BlankLayout',
+    component: '../layouts/SecurityLayout',
     routes: [
       {
         path: '/user',
@@ -14,44 +14,35 @@ export default [
       },
       {
         path: '/',
-        component: '../layouts/SecurityLayout',
+        component: '../layouts/BasicLayout',
         routes: [
+          { path: '/', redirect: '/welcome' },
+          { path: '/welcome', name: 'welcome', icon: 'smile', component: './Welcome' },
+          // 系统管理
           {
-            path: '/',
-            component: '../layouts/BasicLayout',
+            path: '/sys',
+            name: 'sys',
+            icon: 'setting',
             routes: [
-              { path: '/welcome', name: 'welcome', icon: 'smile', component: './Welcome' },
-              // 系统管理
-              {
-                path: '/sys',
-                name: 'sys',
-                icon: 'setting',
-                routes: [
-                  {
-                    path: '/sys/organization',
-                    name: 'organization',
-                    component: './sys/organization',
-                  },
-                  { path: '/sys/account', name: 'account', component: './sys/account' },
-                  { path: '/sys/module', name: 'module', component: './sys/module' },
-                  { path: '/sys/role', name: 'role', component: './sys/role' },
-                  { path: '/sys/dictionary', name: 'dictionary', component: './sys/dictionary' },
-                ],
-              },
-              // 个人中心
-              {
-                path: '/config',
-                name: 'user',
-                routes: [
-                  { name: 'setting', path: '/config/settings', component: './user/settings' },
-                  { name: 'center', path: '/config/center', component: './user/center/' },
-                ],
-              },
+              { path: '/sys/organization', name: 'organization', component: './sys/organization' },
+              { path: '/sys/account', name: 'account', component: './sys/account' },
+              { path: '/sys/module', name: 'module', component: './sys/module' },
+              { path: '/sys/role', name: 'role', component: './sys/role' },
+              { path: '/sys/dictionary', name: 'dictionary', component: './sys/dictionary' },
             ],
           },
-          { component: '404' },
+          // 个人中心
+          {
+            path: '/config',
+            name: 'user',
+            routes: [
+              { name: 'setting', path: '/config/settings', component: './user/settings' },
+              { name: 'center', path: '/config/center', component: './user/center/' },
+            ],
+          },
         ],
       },
+      { component: '404' },
     ],
   },
 ];
