36802d26272076b549d7871b68b23fe5536337db
PKAQ <pkaq@msn.com>
2019-10-26 15:18:56 +0800

    请求路径调整

--- src/pages/sys/dictionary/services/dictSvc.js
+++ src/pages/sys/dictionary/services/dictSvc.js
@@ -3,12 +3,12 @@ import { getNoUndefinedString } from '@src/utils/utils';
 
 // 查询字典列表
 export async function listDict() {
-  return request('/api/sys/dict/list');
+  return request('/api/sys/dictionary/list');
 }
 
 // 新增/编辑字典
 export async function editDict(params) {
-  return request('/api/sys/dict/edit', {
+  return request('/api/sys/dictionary/edit', {
     method: 'POST',
     data: {
       ...params,
@@ -18,10 +18,10 @@ export async function editDict(params) {
 
 // 查询字典分类
 export async function getDict(params) {
-  return request(`/api/sys/dict/get/${getNoUndefinedString(params.id)}`);
+  return request(`/api/sys/dictionary/get/${getNoUndefinedString(params.id)}`);
 }
 
 // 删除字典
 export async function deleteDict(params) {
-  return request(`/api/sys/dict/del/${getNoUndefinedString(params.id)}`);
+  return request(`/api/sys/dictionary/del/${getNoUndefinedString(params.id)}`);
 }
