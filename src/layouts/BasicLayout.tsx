0d9b453637b1c9eb65a7c9b428e7283e1c886b95
PKAQ <pkaq@msn.com>
2019-11-07 09:18:17 +0800

    标题的改

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -50,7 +50,7 @@ const footerRender: BasicLayoutProps['footerRender'] = () => (
         textAlign: 'center',
       }}
     >
-      Copyright <Icon type="copyright" /> 2019 By Nerv
+      Copyright <Icon type="copyright" /> 2019 By PKAQ
     </div>
   </>
 );
--- src/models/login.ts
+++ src/models/login.ts
@@ -47,23 +47,22 @@ const Model: LoginModelType = {
       });
       // Login successfully
       if (response && response.success) {
-        console.debug(' login successful ... ');
-        const urlParams = new URL(window.location.href);
+        // const urlParams = new URL(window.location.href);
         const params = getPageQuery();
-        let { redirect } = params as { redirect: string };
+        const { redirect } = params as { redirect: string };
         if (redirect) {
-          const redirectUrlParams = new URL(redirect);
-          if (redirectUrlParams.origin === urlParams.origin) {
-            redirect = redirect.substr(urlParams.origin.length);
-            if (redirect.match(/^\/.*#/)) {
-              redirect = redirect.substr(redirect.indexOf('#') + 1);
-            }
-          } else {
-            window.location.href = redirect;
-            return;
-          }
+          // const redirectUrlParams = new URL(redirect);
+          // if (redirectUrlParams.origin === urlParams.origin) {
+          //   redirect = redirect.substr(urlParams.origin.length);
+          //   if (redirect.match(/^\/.*#/)) {
+          //     redirect = redirect.substr(redirect.indexOf('#') + 1);
+          //   }
+          // } else {
+          window.location.href = redirect;
+          //   return;
+          // }
         }
-        yield put(routerRedux.replace(redirect || '/'));
+        // yield put(routerRedux.replace(redirect || '/'));
       }
     },
     *logout(_, { put }) {
