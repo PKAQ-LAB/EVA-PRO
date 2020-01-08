09dd11e3e9a465dbcd3531efea535b582d32d03a
PKAQ <pkaq@msn.com>
2020-01-08 16:09:44 +0800

    样式调整

--- src/pages/sys/dictionary/index.jsx
+++ src/pages/sys/dictionary/index.jsx
@@ -13,8 +13,8 @@ export default class Dict extends React.PureComponent {
   render() {
     return (
       <PageHeaderWrapper title="字典管理" subTitle="系统字典（码表）管理维护">
-        <div style={{ padding: 15 }}>
-          <SideLayout title="字典管理" width={400} bodyStyle={{ padding: 0 }} body={<List />}>
+        <div className="eva-body">
+          <SideLayout title="字典管理" width={400} bodyStyle={{ padding: 0 }}  layoutStyle={{ minHeight: 'calc(100vh - 215px)' }} body={<List />}>
             <AOEForm />
           </SideLayout>
         </div>
