b94d9b12eab0d534ecf54df1fa9151fcf0cefa7d
PKAQ <pkaq@msn.com>
2020-02-19 11:30:21 +0800

    添加头像

--- src/components/GlobalHeader/AvatarDropdown.tsx
+++ src/components/GlobalHeader/AvatarDropdown.tsx
@@ -55,10 +55,12 @@ class AvatarDropdown extends React.Component<GlobalHeaderRightProps> {
       </Menu>
     );
 
+    const defaultAvatar = "https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png";
+
     return currentUser && currentUser.name ? (
       <HeaderDropdown overlay={menuHeaderDropdown}>
         <span className={`${styles.action} ${styles.account}`}>
-          <Avatar size="small" className={styles.avatar} src={currentUser.avatar} alt="avatar" />
+          <Avatar size="small" className={styles.avatar} src={currentUser.avatar || defaultAvatar} alt="avatar" />
           <span className={styles.name}>{currentUser.name}</span>
         </span>
       </HeaderDropdown>
