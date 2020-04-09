e9eb794013fc6987a0f22d7fb5b6532c9c3a3a61
PKAQ <pkaq@msn.com>
2020-04-09 20:55:47 +0800

    update

--- src/pages/pdos/sale/slip/services/slipSvc.js
+++ src/pages/pdos/sale/slip/services/slipSvc.js
@@ -4,17 +4,17 @@ import { getNoUndefinedString } from '@src/utils/utils';
 
 // 销售单列表
 export async function list(params) {
-  return request(`/api/sale/list?${stringify(params)}`);
+  return request(`/api/pdos/sale/slip/list?${stringify(params)}`);
 }
 
 // 获取销售单详情
 export async function get(params) {
-  return request(`/api/sale/get/${getNoUndefinedString(params.id)}`);
+  return request(`/api/pdos/sale/slip/get/${getNoUndefinedString(params.id)}`);
 }
 
 // 新增/编辑组织信息
 export async function editSlip(params) {
-  return request('/api/sale/edit', {
+  return request('/api/pdos/sale/slip/edit', {
     method: 'POST',
     data: {
       ...params,
@@ -23,7 +23,7 @@ export async function editSlip(params) {
 }
 // 根据ID删除角色
 export async function delSlip(params) {
-  return request('/api/sale/del', {
+  return request('/api/pdos/sale/slip/del', {
     method: 'POST',
     data: {
       ...params,
