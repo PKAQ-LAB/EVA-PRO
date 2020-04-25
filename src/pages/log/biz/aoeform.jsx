fc16ada1a3efbfdb620a4e70840eb224c6b85901
PKAQ <pkaq@msn.com>
2020-04-25 10:35:24 +0800

    使用hooks重构

--- src/pages/log/biz/aoeform.jsx
+++ src/pages/log/biz/aoeform.jsx
@@ -1,53 +1,38 @@
 /* eslint-disable jsx-a11y/label-has-for */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { Component } from 'react';
+import React from 'react';
 import { Drawer } from 'antd';
-import { connect } from 'umi';
 import moment from 'moment';
 import styles from './index.less';
 
-@connect(state => ({
-  bizlog: state.bizlog,
-}))
-export default class AOEForm extends Component {
-  // 关闭窗口
-  handleCloseForm = () => {
-    this.props.dispatch({
-      type: 'bizlog/updateState',
-      payload: {
-        drawerCheck: '',
-      },
-    });
-  };
+export default (props) => {
+  const { currentItem, operateType, setOperateType, } = props;
 
-  render() {
-    const { drawerCheck, currentItemcheck } = this.props.bizlog;
-    return (
-      <Drawer
-        closable
-        width={600}
-        visible={drawerCheck === 'check'}
-        onClose={() => this.handleCloseForm()}
-      >
-        <ul className={styles.ul}>
-          <li>
-            <label>操作时间：</label>
-            <span>{moment(currentItemcheck.operateDatetime).format('MM-DD HH:mm')}</span>
-          </li>
-          <li>
-            <label>操作人：</label>
-            <span>{currentItemcheck.operator}</span>
-          </li>
-          <li>
-            <label>操作类型：</label>
-            <span>{currentItemcheck.operateType}</span>
-          </li>
-          <li>
-            <label>描述：</label>
-            <span>{currentItemcheck.description}</span>
-          </li>
-        </ul>
-      </Drawer>
-    );
-  }
+  return (
+    <Drawer
+      closable
+      width={600}
+      visible={operateType === 'check'}
+      onClose={() => setOperateType("")}
+    >
+      <ul className={styles.ul}>
+        <li>
+          <label>操作时间：</label>
+          <span>{moment(currentItem.operateDatetime).format('MM-DD HH:mm')}</span>
+        </li>
+        <li>
+          <label>操作人：</label>
+          <span>{currentItem.operator}</span>
+        </li>
+        <li>
+          <label>操作类型：</label>
+          <span>{currentItem.operateType}</span>
+        </li>
+        <li>
+          <label>描述：</label>
+          <span>{currentItem.description}</span>
+        </li>
+      </ul>
+    </Drawer>
+  );
 }
--- src/pages/log/biz/index.jsx
+++ src/pages/log/biz/index.jsx
@@ -1,92 +1,39 @@
-import React, { PureComponent } from 'react';
-import moment from 'moment';
-import { connect } from 'umi';
+import React, { useState } from 'react';
 import '@ant-design/compatible/assets/index.css';
-import { Form, Button, Row, Col, Divider, DatePicker } from 'antd';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
+import { useRequest } from '@umijs/hooks';
 import WorkList from './list';
 import AOEForm from './aoeform';
-
-const FormItem = Form.Item;
-const { RangePicker } = DatePicker;
-
-@connect(state => ({
-  bizlog: state.bizlog,
-  loading: state.loading.models.bizlog,
-}))
-export default class Workdaily extends PureComponent {
-  formRef = React.createRef();
-
-  // 重置
-  handleFormReset = () => {
-    this.formRef.current.resetFields();
-    this.props.dispatch({
-      type: 'bizlog/fetch',
-    });
-  };
-
-  // 查询
-  handleSearch = () => {
-    const { dispatch } = this.props;
-    const { validateFields } = this.formRef.current;
-
-    validateFields().then(values => {
-      const data = {
-        ...values,
-      };
-
-      const param = {};
-      param.begin = data.operateDatetime
-        ? moment(data.operateDatetime[0]).format('YYYY-MM-DD')
-        : '';
-      param.end = data.operateDatetime ? moment(data.operateDatetime[1]).format('YYYY-MM-DD') : '';
-
-      dispatch({
-        type: 'bizlog/fetch',
-        payload: param,
-      });
-    });
-  };
+import { list } from './service/bizSvc';
 
 
-  // 查询条件渲染
-  renderSimpleForm() {
-    const { loading } = this.props;
+export default () => {
+  const [operateType, setOperateType] = useState("");
+  const [currentItem, setCurrentItem] = useState({});
 
-    return (
-      <Form layout="inline" initialValues={{"operateDatetime":[moment().add(-7, 'days'), moment()]}} ref={this.formRef}>
-        <Row type="flex" justify="space-between">
-          <Col>
-            <FormItem label="操作时间" name="operateDatetime">
-              <RangePicker />
-            </FormItem>
-          </Col>
-          <Col>
-            <FormItem>
-              <Button type="primary" onClick={this.handleSearch} loading={loading}>
-                查询
-              </Button>
-              <Divider type="vertical" />
-              <Button onClick={this.handleFormReset} loading={loading}>
-                重置
-              </Button>
-            </FormItem>
-          </Col>
-        </Row>
-      </Form>
-    );
+  const { run, tableProps } = useRequest(list, {
+    paginated: true,
+    formatResult: (res) => {
+      return res.data;
+    }
+  })
+
+  const formProps = {
+    currentItem, operateType, setOperateType,
   }
 
-  render() {
-    const { drawerCheck } = this.props.bizlog;
-    return (
-      <PageHeaderWrapper>
-        <div className="eva-ribbon">{this.renderSimpleForm()}</div>
-        <div className="eva-body">
-          <WorkList />
-        </div>
-        {drawerCheck && <AOEForm />}
-      </PageHeaderWrapper>
-    );
+   // 列表属性
+   const listProps = {
+    fetch: run,
+    setCurrentItem,
+    operateType,
+    setOperateType,
+    tableProps,
   }
+  return (
+    <PageHeaderWrapper>
+      <WorkList {...listProps} />
+      {operateType !== '' && <AOEForm {...formProps} />}
+    </PageHeaderWrapper>
+  );
 }
