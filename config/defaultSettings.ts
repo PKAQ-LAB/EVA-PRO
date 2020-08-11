8ae1d4a546e7874f958effbb986b724fd0e2a5fa
PKAQ <pkaq@msn.com>
2020-08-11 17:24:21 +0800

    添加配置项

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -1,18 +1,26 @@
 import { Settings as LayoutSettings } from '@ant-design/pro-layout';
 
 export default {
-  navTheme: 'light',
+  version: '1.0.0',
+  url: 'http://localhost:9016/api/',
+  imgUrl: 'http://localhost:9016/img/',
+  token_key: 'auth_token',
+  user_key: 'user_info',
+  subTile: '基于Ant Design Pro的后台管理方案',
+  copyright: 'Power by PKAQ © 2019',
+  // 自定义属性结束
+  navTheme: 'dark',
   // 拂晓蓝
   primaryColor: '#1890ff',
   layout: 'mix',
   contentWidth: 'Fluid',
-  fixedHeader: false,
-  fixSiderbar: true,
+  fixedHeader: true,
+  fixSiderbar: false,
   colorWeak: false,
   menu: {
     locale: true,
   },
-  title: 'Ant Design Pro',
+  title: 'EVA Pro',
   pwa: false,
   iconfontUrl: '',
 } as LayoutSettings & {
