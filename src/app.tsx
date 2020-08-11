e675ad1de3d8423761e6d1074770d40e94cec817
PKAQ <pkaq@msn.com>
2020-08-11 18:04:18 +0800

    请求处理

--- src/app.tsx
+++ src/app.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BasicLayoutProps, Settings as LayoutSettings } from '@ant-design/pro-layout';
 import { notification } from 'antd';
 import { history, RequestConfig } from 'umi';
+import { RequestOptionsInit } from 'umi-request';
 import RightContent from '@/components/RightContent';
 import Footer from '@/components/Footer';
 import { ResponseError } from 'umi-request';
 import { queryCurrent } from './services/user';
 import defaultSettings from '../config/defaultSettings';
+import { message } from 'antd';
 
 export async function getInitialState(): Promise<{
   currentUser?: API.CurrentUser;
@@ -49,38 +51,32 @@ export const layout = ({
   };
 };
 
-const codeMessage = {
-  200: '服务器成功返回请求的数据。',
-  201: '新建或修改数据成功。',
-  202: '一个请求已经进入后台排队（异步任务）。',
-  204: '删除数据成功。',
-  400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
-  401: '用户没有权限（令牌、用户名、密码错误）。',
-  403: '用户得到授权，但是访问是被禁止的。',
-  404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
-  405: '请求方法不被允许。',
-  406: '请求的格式不可得。',
-  410: '请求的资源被永久删除，且不会再得到的。',
-  422: '当创建一个对象时，发生一个验证错误。',
-  500: '服务器发生错误，请检查服务器。',
-  502: '网关错误。',
-  503: '服务不可用，服务器暂时过载或维护。',
-  504: '网关超时。',
-};
-
 /**
  * 异常处理程序
  */
 const errorHandler = (error: ResponseError) => {
   const { response } = error;
   if (response && response.status) {
-    const errorText = codeMessage[response.status] || response.statusText;
     const { status, url } = response;
 
-    notification.error({
-      message: `请求错误 ${status}: ${url}`,
-      description: errorText,
-    });
+    if (status === 401) {
+      // @HACK
+      /* eslint-disable no-underscore-dangle */
+      window.g_app._store.dispatch({
+        type: 'login/logout',
+      });
+      return;
+    }
+
+    if (status === 403) {
+      history.push('/exception/403');
+    } else if (status >= 404 && status < 422) {
+      history.push('/exception/404');
+    } else {
+      message.error({
+        content: '未知网络错误，无法连接服务器',
+      });
+    }
   }
 
   if (!response) {
@@ -92,6 +88,52 @@ const errorHandler = (error: ResponseError) => {
   throw error;
 };
 
+// request拦截器, 改变url 或 options.
+const requestInterceptors = (url: string, options: RequestOptionsInit) => {
+  // eslint-disable-next-line no-param-reassign
+  const header = {
+    ...options.headers,
+    Accept: 'application/json',
+    'Content-Type': 'application/json; charset=utf-8',
+    Pragma: 'no-cache',
+    'Cache-Control': 'no-cache',
+    'device': 'pc',
+    'version': defaultSettings.version
+  };
+
+  return {
+    url,
+    options: {
+      ...options,
+      header,
+      interceptors: true,
+    },
+  };
+};
+
+/**
+ * 4. 基于response interceptors
+ */
+const responseInterceptors = (response: Response, options: RequestOptionsInit) => {
+  const result = response.clone().json();
+  if (result && result.success) {
+    if (result.message) {
+      message.success(result.message);
+    }
+  } else {
+    message.error(result.message || '操作失败');
+  }
+
+  return response;
+};
+
+
 export const request: RequestConfig = {
+  useCache: false,
+  // 默认错误处理
   errorHandler,
+  // 默认请求是否带上cookie
+  credentials: 'include',
+  requestInterceptors: [requestInterceptors],
+  responseInterceptors: [responseInterceptors]
 };
