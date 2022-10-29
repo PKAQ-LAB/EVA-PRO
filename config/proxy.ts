1f79bcf9e3721e82dd496c5be638d9450d6a2bb5
PKAQ <pkaq@msn.com>
2022-10-29 17:20:12 +0800

    style fix

--- config/proxy.ts
+++ config/proxy.ts
@@ -14,7 +14,7 @@ export default {
     // localhost:8000/api/** -> https://preview.pro.ant.design/api/**
     '/api/': {
       // 要代理的地址
-      target: 'https://preview.pro.ant.design',
+      target: 'http://localhost:9016/',
       // 配置了这个可以从 http 代理到 https
       // 依赖 origin 的功能可能需要这个，比如 cookie
       changeOrigin: true,
