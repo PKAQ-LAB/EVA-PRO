0f616353a6cdf669bdc126ec7dbc7082d8730c52
PKAQ <pkaq@msn.com>
2022-11-20 16:04:03 +0800

    E:JS to TS

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -3,18 +3,18 @@ import { request } from '@umijs/max';
 import { getNoUndefinedString } from '@/utils/utils';
 
 // get请求
-async function get(url, params) {
+async function get(url: string, params: string) {
   return request(`${url}/${getNoUndefinedString(params)}`);
 }
 
 // 加载列表
-async function list(url, params) {
-  const urls =  params? `${url}?${stringify(params)}` : url;
+async function list(url: string, params: object) {
+  const urls = params ? `${url}?${stringify(params)}` : url;
   return request(urls);
 }
 
 // 新增/编辑
-export async function edit(url,params) {
+export async function edit(url: string, params: object) {
   return request(url, {
     method: 'POST',
     data: {
@@ -23,7 +23,7 @@ export async function edit(url,params) {
   });
 }
 // 根据ID删除
-export async function del(url,params) {
+export async function del(url: string, params: object) {
   return request(url, {
     method: 'POST',
     data: {
@@ -33,7 +33,7 @@ export async function del(url,params) {
 }
 
 // post请求
-async function post(url, params) {
+async function post(url: string, params: object) {
   return request(url, {
     method: 'POST',
     data: params,
@@ -43,5 +43,6 @@ async function post(url, params) {
 export default {
   get,
   list,
-  post
-}
+  del,
+  post,
+};
