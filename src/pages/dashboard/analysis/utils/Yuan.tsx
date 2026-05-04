2fdb1b577d22dd68ee095eb1c185a0624c7c49e3
PKAQ <pkaq@msn.com>
2026-05-04 09:48:49 +0800

    feat: 使用simple模式

--- src/pages/dashboard/analysis/utils/Yuan.tsx
+++
@@ -1,7 +0,0 @@
-import { formatYuan } from '@/utils/format';
-
-const Yuan: React.FC<{ children: string | number }> = ({ children }) => {
-  return <span>{formatYuan(children)}</span>;
-};
-
-export default Yuan;
