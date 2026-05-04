960704b24569e4a09ff9ee85e17b44cc2cd9ac71
PKAQ <pkaq@msn.com>
2026-05-04 21:14:31 +0800

    feat: 修改title

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -19,7 +19,7 @@ const Settings: ProLayoutProps & {
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
-  title: 'Ant Design Pro',
+  title: 'Eva Admin Pro',
   subTitle: 'This is subTitle',
   copyright: `Power by PKAQ © ${new Date().getFullYear()}`,
   version: '6.0',
