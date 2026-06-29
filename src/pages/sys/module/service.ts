f6a585076d7aba6ac7c9d8bafb5e2f5f191a2782
PKAQ <pkaq@msn.com>
2026-06-29 12:16:36 +0800

    feat: 对接系统管理后台接口

--- src/pages/sys/module/service.ts
+++ src/pages/sys/module/service.ts
@@ -1,5 +1,11 @@
 import APIS from '@/apis';
 import http from '@/utils/http';
+import {
+  enabledStatusToFrozen,
+  frozenToEnabledStatus,
+  normalizeTree,
+  toBackendTreePayload,
+} from '../adapter';
 import type { ModuleItem, ModuleStatus } from './data.d';
 
 export interface ModuleListResponse {
@@ -13,19 +19,35 @@ export interface MutationResult {
 }
 
 export const queryModules = (params?: Record<string, unknown>) =>
-  http.list<ModuleListResponse>(APIS.MODULE_LIST, params);
+  http.list<ModuleListResponse>(APIS.MODULE_LIST, params).then((res) => ({
+    ...res,
+    data: normalizeTree(res.data, frozenToEnabledStatus),
+  }));
 
 export const getModule = (id: string) =>
-  http.get<{ data?: ModuleItem; success?: boolean }>(APIS.MODULE_GET, id);
+  http
+    .get<{ data?: ModuleItem; success?: boolean }>(APIS.MODULE_GET, id)
+    .then((res) => ({
+      ...res,
+      data: res.data
+        ? normalizeTree([res.data], frozenToEnabledStatus)[0]
+        : undefined,
+    }));
 
 export const editModule = (data: Partial<ModuleItem>) =>
-  http.post<MutationResult>(APIS.MODULE_EDIT, data);
+  http.post<MutationResult>(
+    APIS.MODULE_EDIT,
+    toBackendTreePayload(data as ModuleItem, 'status', enabledStatusToFrozen),
+  );
 
 export const deleteModules = (ids: string[]) =>
   http.post<MutationResult>(APIS.MODULE_DEL, { param: ids });
 
 export const switchModuleStatus = (id: string, status: ModuleStatus) =>
-  http.post<MutationResult>(APIS.MODULE_STATUS, { id, status });
+  http.post<MutationResult>(APIS.MODULE_STATUS, {
+    param: [id],
+    frozen: enabledStatusToFrozen(status),
+  });
 
 export const sortModules = (rows: Array<{ id: string; orders: number }>) =>
   http.post<MutationResult>(APIS.MODULE_SORT, rows);
