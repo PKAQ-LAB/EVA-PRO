6e1317928dcd9c7e16e011c60866133065f1791a
PKAQ <pkaq@msn.com>
2026-06-29 12:17:26 +0800

    feat: 优化登录页与头部交互

--- src/components/BizIcon/index.tsx
+++ src/components/BizIcon/index.tsx
@@ -5,7 +5,7 @@ import { createFromIconfontCN } from '@ant-design/icons';
  * 用法：<BizIcon type="icon-xxx" />
  */
 const BizIcon = createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/font_597299_cfjebjaqyc0rizfr.css',
+  scriptUrl: '//at.alicdn.com/t/font_597299_cfjebjaqyc0rizfr.js',
 });
 
 export default BizIcon;
